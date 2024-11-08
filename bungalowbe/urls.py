9feb7f9315ba0d379c85a0831bae84d8ec01e5ab
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2024-11-08 17:36:00 +0530

    Update urls.py

--- bungalowbe/urls.py
+++ bungalowbe/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def static_response(request):
+    return HttpResponse("You're at the bungalowbe index.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("core/", include("core.urls")),
+    path("", static_response), 
 ]

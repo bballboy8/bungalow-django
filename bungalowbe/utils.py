6559df3407f7bed5261046e89af5d0f3ab556a54
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2024-11-09 15:05:50 +0530

    Update utils.py

--- bungalowbe/utils.py
+++ bungalowbe/utils.py
@@ -1,8 +1,11 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import pytz
 
 def get_utc_time():
     return datetime.now(timezone.utc)
+
+def get_x_days_ago_utc_time(x):
+    return get_utc_time() - timedelta(days=x)
 
 def convert_iso_to_datetime(iso_string):
     dt = datetime.fromisoformat(iso_string)

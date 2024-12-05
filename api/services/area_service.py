82a5d2cc0f0b7b316a16daf4157ffdab304b3529
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2024-12-05 20:16:27 +0530

    Get Selection Pin analytics

--- api/services/area_service.py
+++ api/services/area_service.py
@@ -8,6 +8,9 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from core.utils import s3, bucket_name
 from typing import List
+from datetime import datetime, timedelta
+from django.db.models import Avg
+
 
 
 def convert_geojson_to_wkt(geometry):
@@ -112,3 +115,61 @@ def group_by_vendor(data):
         if vendor_name and not item.get("image_uploaded"):
             grouped_data.setdefault(vendor_name, []).append(item['vendor_id'])
     return grouped_data
+def get_pin_selection_analytics_and_location(latitude: float, longitude: float, distance: float, duration: int = 1):
+    """
+    Get the analytics and location of the selected pin.
+
+    Args:
+        latitude (float): Latitude of the selected pin.
+        longitude (float): Longitude of the selected pin.
+        distance (float): Distance in kilometers.
+
+    Returns:
+        dict: A dictionary containing analytics and location data.
+    """
+    try:
+        # Required oldest date, new date, Total count of images, Average per day
+        logger.info("Inside get pin selection analytics and location service")
+        func_start_time = datetime.now()
+
+        # Create a geospatial point object from the provided latitude and longitude
+        point = Point(longitude, latitude, srid=4326)
+        buffered_polygon = point.buffer(distance / 111.32)  # 1 degree ≈ 111.32 km
+
+        # Get the start time based on the selected duration
+        start_time = datetime.now() - timedelta(days=duration)
+
+        logger.info(f"Latitude: {latitude}, Longitude: {longitude}, Distance: {distance}, Duration: {duration}")
+
+        # Construct the query filtering by location and the selected time range
+        captures = SatelliteCaptureCatalog.objects.filter(
+                location_polygon__intersects=buffered_polygon,
+                acquisition_datetime__gte=start_time
+            )
+
+        # Return the analytics (count of captures)
+        analytics = {
+            "count": captures.count(),
+            "average_per_day": captures.count() / duration,
+            "oldest_date": captures.order_by("acquisition_datetime").first(),
+            "newest_date": captures.order_by("acquisition_datetime").last(),
+        }
+
+        if analytics["oldest_date"]:
+            analytics["oldest_date"] = analytics["oldest_date"].acquisition_datetime
+        if analytics["newest_date"]:
+            analytics["newest_date"] = analytics["newest_date"].acquisition_datetime
+
+        func_end_time = datetime.now()
+        logger.info(f"Time taken to fetch pin selection analytics and location: {func_end_time - func_start_time}")
+
+        logger.info("Pin selection analytics and location fetched successfully")
+        return {
+            "data": {"analytics": analytics},
+            "status_code": 200,
+        }
+
+    except Exception as e:
+        logger.error(f"Error fetching pin selection analytics and location: {str(e)}")
+        return {"data": f"{str(e)}", "status_code": 400}
+    

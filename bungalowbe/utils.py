6b0a262d070a24c9345275ee64eab144c639c223
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2025-02-14 11:13:15 +0530

    update in files

--- bungalowbe/utils.py
+++ bungalowbe/utils.py
@@ -31,7 +31,7 @@ def reverse_geocode_shapefile(lat, lon):
         base_dir = os.getcwd() 
         lat, lon = float(lat), float(lon)
         # Construct absolute paths dynamically
-        states_shapefile = os.path.join(base_dir, "static", "shapesFiles", "state_provinces", "ne_110m_admin_1_states_provinces.shp")
+        states_shapefile = os.path.join(base_dir, "static", "shapesFiles", "state_provinces", "ne_10m_admin_1_states_provinces.shp")
         marine_shapefile = os.path.join(base_dir, "static", "shapesFiles", "marine_polys", "ne_10m_geography_marine_polys.shp")
 
         # check if the shapefiles exist
@@ -43,7 +43,7 @@ def reverse_geocode_shapefile(lat, lon):
 
         point = Point(lon, lat)
 
-        match = states[states.geometry.contains(point)]
+        match = states[states.geometry.intersects(point)]
         if not match.empty:
             # print(", ".join(map(str, match.columns)))
             # print(", ".join(map(str, match.iloc[0].values)))
@@ -52,7 +52,7 @@ def reverse_geocode_shapefile(lat, lon):
             local = match.iloc[0]["gn_name"]
             return region, local
 
-        match = marine[marine.geometry.contains(point)]
+        match = marine[marine.geometry.intersects(point)]
         if not match.empty:
             region = match.iloc[0]["name_en"]
             local = f"{lat}, {lon}"
@@ -64,5 +64,5 @@ def reverse_geocode_shapefile(lat, lon):
         return "Unknown", f"{lat}, {lon}"
 
 
-#  from core.utils import reverse_geocode_shapefile
+#  from bungalowbe.utils import reverse_geocode_shapefile
 #  reverse_geocode_shapefile(34.0549, 118.2426)

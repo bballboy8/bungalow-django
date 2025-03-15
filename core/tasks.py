575b391bc6422a28f18bd84880fb3ef32d80ec50
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2025-03-15 13:56:48 +0530

    Airbus Product Orders

--- core/tasks.py
+++ core/tasks.py
@@ -1,7 +1,7 @@
 # core/tasks.py
 from celery import shared_task
 from core.services.blacksky_catalog_api import run_blacksky_catalog_api, run_blacksky_catalog_bulk_api_for_last_35_days_from_now, fetch_and_process_products_records
-from core.services.airbus_catalog_api import run_airbus_catalog_api, run_airbus_catalog_api_bulk_for_last_35_days_from_now
+from core.services.airbus_catalog_api import run_airbus_catalog_api, run_airbus_catalog_api_bulk_for_last_35_days_from_now, fetch_and_process_airbus_products_records
 from core.services.planet_catalog_api import run_planet_catalog_api, run_planet_catalog_bulk_api_for_last_35_days_from_now
 from core.services.capella_master_collector import run_capella_catalog_api, run_capella_catalog_bulk_api_for_last_35_days_from_now
 from core.services.skyfi_catalog_api import run_skyfi_catalog_api, run_skfyfi_catalog_api_bulk_for_last_35_days_from_now
@@ -14,7 +14,6 @@ from api.services.group_and_sites_service import check_updates_in_notification_e
 def run_all_catalogs():
     try:
         run_blacksky_catalog_api()
-        fetch_and_process_products_records()
     except Exception as e:
         print(f"Error occurred while running BlackSky API: {e}")
 
@@ -48,6 +47,19 @@ def run_all_catalogs():
     except Exception as e:
         print(f"Error occurred while checking updates in notification enabled groups: {e}")
 
+
+@shared_task
+def run_fetch_and_process_product_orders():
+    try:
+        fetch_and_process_products_records()
+    except Exception as e:
+        print(f"Error occurred while running BlackSky API: {e}")
+
+    try:
+        fetch_and_process_airbus_products_records()
+    except Exception as e:
+        print(f"Error occurred while running Airbus API: {e}")
+        
 @shared_task
 def run_skyfi_umbra_catalog():
     try:
